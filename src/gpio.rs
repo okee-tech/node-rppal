ff077ea5b4ce9c56fb3534fdfde006d46f1a768b
Demid Kaidalov <demid.kaidalov@gmail.com>
2024-12-04 11:36:52 +0200

    Add GPIO module with sum2 function

---
+++ src/gpio.rs
@@ -0,0 +1,4 @@
+#[napi]
+pub fn sum2(a: i32, b: i32) -> i32 {
+  a + b
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-#![deny(clippy::all)]
-
 #[macro_use]
 extern crate napi_derive;
+
+pub mod gpio;
+pub mod i2c;
+pub mod pwm;
+pub mod spi;
+pub mod uart;
+
+use rppal;
 
 #[napi]
 pub fn sum(a: i32, b: i32) -> i32 {
